426d77345e9b118f0013da5cac334b6f626dd466
Kadir Buğra ATAKUL <bugra.atkl@gmail.com>
2025-10-14 13:49:34 +0300

    godot v3.6 update
    iki boyutlu oyunumun ana menü görüntüsünü ve menünün kodlarını yaptım .

---
+++ AnaMenu.gd
@@ -0,0 +1,20 @@
+extends Control
+
+func _ready():
+	
+	
+	get_tree().paused = false
+	pass 
+
+
+
+
+func _on_btn_oyna_pressed():
+	get_tree().change_scene("res://Sahneler/Level1.tscn")
+	pass
+
+
+func _on_btn_exit_pressed():
+	
+	get_tree().quit()
+	pass
